4e7c2a1be11dbcd9a0cd89ee3376afd71063ef21
tareq <tareq.rafed22@gmail.com>
2021-06-29 20:01:50 +0300

    edited sass files for better responsivness

--- src/pages/home/js/home.js
+++ src/pages/home/js/home.js
@@ -15,20 +15,22 @@ import Background from './bg'
 const Home = () => {
 
 	return (
-		
-			<div className={Style.Container}>
-				<Background>
+
+		<div className={Style.Container}>
+			<Background>
 				<div className={Style.logoContainer}><Logo /></div>
-				</Background>
+			</Background>
+			<div className={Style.menu}>
 				<BlinkingAnimation>
 					<div className={`${Style.textCenter} ${Style.inspect}`}>
 						<span className={Style.clickable}>Inspect</span>
 					</div>
 				</BlinkingAnimation>
 				<div className={Style.textCenter}>Teleporter</div>
-				<div className={Style.tutorial}>Tutorial</div>
 			</div>
-	
+			<div className={Style.tutorial}>Tutorial</div>
+		</div>
+
 	);
 }
 
