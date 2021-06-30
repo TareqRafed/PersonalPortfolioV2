376b4d4f957729818c2572f94e342018efdc6a84
tareq <tareq.rafed22@gmail.com>
2021-07-01 01:15:48 +0300

    changed webpack configurations to serve icons, manifest in development

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devServer: {
-  host: '0.0.0.0',
-  useLocalIp: true,
-},
+    host: '0.0.0.0',
+    useLocalIp: true,
+    contentBase: path.join(__dirname, 'src/public'),
+  },
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
@@ -16,69 +18,68 @@ module.exports = {
       react: path.join(__dirname, 'node_modules', 'react'),
     },
     fallback: {
-            "fs": false
-        },
+      "fs": false
+    },
   },
   module: {
-    
-    rules: 
-    [
-      
-     {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-            {
-            loader: 'file-loader',
-            },
-        ],
-          },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-            options: { 
-              sassOptions:{
-                module:true
-              }
-             }
-          }
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-      {
-      test: /\.(woff|woff2|ttf|otf)$/,
-      loader: 'file-loader',
-      include: [/fonts/],
 
-      options: {
-        name: '[hash].[ext]',
-        outputPath: 'css/',
-        publicPath: url => '../css/' + url
-      }
-    },
-    ],
+    rules:
+      [
+
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  module: true
+                }
+              }
+            }
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+        {
+          test: /\.(woff|woff2|ttf|otf)$/,
+          loader: 'file-loader',
+          include: [/fonts/],
+
+          options: {
+            name: '[hash].[ext]',
+            outputPath: 'css/',
+            publicPath: url => '../css/' + url
+          }
+        },
+      ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/public/index.html',
-      manifest: "./manifest.json",
     }),
   ],
 };
