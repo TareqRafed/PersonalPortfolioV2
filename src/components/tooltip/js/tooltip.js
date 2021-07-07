bb9c2bc1f97218c09bc11988b2b7b0c6089844fb
tareq <tareq.rafed22@gmail.com>
2021-07-07 23:48:34 +0300

    fixed a bug in the message component

--- src/components/tooltip/js/tooltip.js
+++ src/components/tooltip/js/tooltip.js
@@ -3,6 +3,8 @@ import Style from '../style/tooltip.module.scss';
 import PropTypes from 'prop-types';
 
 
+
+// The parent element must be either a fullscreen absolute, or any other position 
 const Tooltip = ({children, tip}) => {
 	const [ShowTip, setShowTip] = useState(false);
 	const [MousePos, setMousePos] = useState({"x":0, "y":0})
@@ -11,17 +13,17 @@ const Tooltip = ({children, tip}) => {
 
 		console.log("render")
 		const handleMovment = (e) =>{
-			setMousePos({"x":e.clientX, "y":e.clientY})
+			setMousePos({"x":e.screenX, "y":e.screenY})
 		}
-		document.addEventListener("mousemove", handleMovment)
+		window.addEventListener("mousemove", handleMovment)
 
 		return () => {
-			document.removeEventListener("mousemove", handleMovment);
+			window.removeEventListener("mousemove", handleMovment);
 		}
 	}, [ShowTip])
 	return(
 		<span style={{cursor:"pointer"}} onMouseEnter={()=> setShowTip(true)} onMouseLeave={()=>setShowTip(false)}>
-			{ShowTip && <span id="tooltip" style={{left:MousePos.x, top:MousePos.y, transform:"translate(-100%,-100%)", zIndex:100}} className={Style.box}>{tip}</span>}
+			{ShowTip && <span id="tooltip" style={{left:MousePos.x, top:MousePos.y, transform:"translate(-100%,-400%)", zIndex:100}} className={Style.box}>{tip}</span>}
 			{children}
 		</span>
 	);
