34c99505d6d4862eb9ffe9d146309c77c3594ba3
tareq <tareq.rafed22@gmail.com>
2021-07-17 23:22:38 +0300

    Worked on projects page

--- src/pages/projects/js/projects.js
+++ src/pages/projects/js/projects.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
-
+import ProjectsIcon from '../../../assets/images/projects.svg';
+import Style from '../style/projects.modules.scss'
 
 const Projects = () => {
 	return(
 		<div>
-			Hello
+			<div className={Style.projectsIconContainer}>
+				<ProjectsIcon />
+			</div>
+			<div>
+				<div className={Style.Project}>x</div>
+				<div className={Style.Project}></div>
+				<div className={Style.Project}></div>
+				<div className={Style.Project}></div>
+			</div>
 		</div>
 	);
 }
